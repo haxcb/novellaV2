f34a73c92b8f84c9f6e07e3c8af20f90b571d074
Hasiba <hasiba_arshad@hotmail.com>
2014-07-25 19:14:56 -0700

    Added times to a ton of dates

--- www/js/services.js
+++ www/js/services.js
@@ -36,18 +36,18 @@ nov.service('Data', function () {
 		enrolledCourses: [
 		{ name: 'CMPT 475', id: 0, instructor: 'Herbert H. Tsang', semester: 'Summer 2014', section: 'E100', role: 'Student',
 			courseMaterials: [
-				{ name: 'Intro to Software Engineering', id: 10, fileType: 'PDF', uploadDate: '2014-05-05', download: true, url: 'test/1.PDF'},
-				{ name: 'Agile Development', id: 11, fileType: 'PDF', uploadDate: '2014-05-12', download: true, url: 'test/1.PDF'},
-				{ name: 'Security', id: 12, fileType: 'PDF', uploadDate: '2014-06-16', url: 'test/1.PDF'},
-				{ name: 'Video tutorial', id:13, fileType: 'video', uploadDate: '2014-07-20', url: 'https://www.youtube.com/watch?v=pYkRc4s1OEg'}
+				{ name: 'Intro to Software Engineering', id: 10, fileType: 'PDF', uploadDate: '2014-05-05', timestamp: '5:59PM', download: true, url: 'test/1.PDF'},
+				{ name: 'Agile Development', id: 11, fileType: 'PDF', uploadDate: '2014-05-12', timestamp: '5:59PM', download: true, url: 'test/1.PDF'},
+				{ name: 'Security', id: 12, fileType: 'PDF', uploadDate: '2014-06-16', timestamp: '5:59PM', url: 'test/1.PDF'},
+				{ name: 'Video tutorial', id:13, fileType: 'video', uploadDate: '2014-07-20', timestamp: '5:59PM', url: 'https://www.youtube.com/watch?v=pYkRc4s1OEg'}
 			],
 			assignments: [
-				{name: 'Project Deliverable 1', id: 21, dueDate: '2014-05-25', totalGrade: 100, weight: 25, uploadDate: '2014-05-05', url: 'test/1.PDF'},
-				{name: 'Report', id: 22, dueDate: '2014-06-15', weight: 30, totalGrade: 80, uploadDate: '2014-06-05', url: 'test/1.PDF'},
-				{name: 'Project Deliverable 2', id: 23, dueDate: '2014-08-20', totalGrade: 10, weight: 30, uploadDate: '2014-07-05', url: 'test/1.PDF'}
+				{name: 'Project Deliverable 1', id: 21, dueDate: '2014-05-25', timeDue: '11:59PM', totalGrade: 100, weight: 25, uploadDate: '2014-05-05', timestamp: '5:59PM', url: 'test/1.PDF'},
+				{name: 'Report', id: 22, dueDate: '2014-06-15', timeDue: '11:59PM', weight: 30, totalGrade: 80, uploadDate: '2014-06-05', url: 'test/1.PDF'},
+				{name: 'Project Deliverable 2', id: 23, dueDate: '2014-08-20', timeDue: '11:59PM', totalGrade: 10, weight: 30, uploadDate: '2014-07-05', timestamp: '5:59PM', url: 'test/1.PDF'}
 			],
 			quizzes: [
-				{name: 'Quiz 1', id: 40, dueDate: '2014-07-30', questions: [ 
+				{name: 'Quiz 1', id: 40, dueDate: '2014-07-30', timeDue: '11:59PM', questions: [ 
 					{value : 'Which value is not part of the Agile Manifesto?', answer: 0, responses : 
 						['Simple requirements over comprehensive documentation', 
 						'People over processes',
@@ -59,33 +59,33 @@ nov.service('Data', function () {
 						'Correct answer here']
 					}
 				]},
-				{name: 'Quiz 2', id: 41, dueDate: '2014-07-30'}
+				{name: 'Quiz 2', id: 41, timeDue: '11:59PM', dueDate: '2014-07-30'}
 			] },
 		{ name: 'CMPT 355', id: 1, instructor: 'Wo Shun Luk', semester: 'Summer 2014', section: 'D100', role: 'Student', 
 			courseMaterials: [
-				{ name: 'Lecture 1', id: 14, fileType: 'PDF', uploadDate: '2014-05-05', download: true, url: 'test/1.PDF'},
-				{ name: 'Lecture 2', id: 15, fileType: 'PDF', uploadDate: '2014-05-12', url: 'test/1.PDF'}
+				{ name: 'Lecture 1', id: 14, fileType: 'PDF', uploadDate: '2014-05-05', timestamp: '5:59PM', download: true, url: 'test/1.PDF'},
+				{ name: 'Lecture 2', id: 15, fileType: 'PDF', uploadDate: '2014-05-12', timestamp: '5:59PM',  url: 'test/1.PDF'}
 			],
 			assignments: [
-				{name: 'Assignment 1', id: 24, dueDate: '2014-05-25', totalGrade: 100, weight: 25, uploadDate: '2014-05-05', url: 'test/1.PDF'},
-				{name: 'Assignment 2', id: 25, dueDate: '2014-08-20', totalGrade: 10, weight: 30, uploadDate: '2014-05-05', url: 'test/1.PDF'}
+				{name: 'Assignment 1', id: 24, dueDate: '2014-05-25', timeDue: '11:59PM', totalGrade: 100, weight: 25, uploadDate: '2014-05-05', timestamp: '5:59PM', url: 'test/1.PDF'},
+				{name: 'Assignment 2', id: 25, dueDate: '2014-08-20', timeDue: '11:59PM', totalGrade: 10, weight: 30, uploadDate: '2014-05-05', timestamp: '5:59PM', url: 'test/1.PDF'}
 			] },
 		{ name: 'ENGL 103w', id: 2, instructor: 'Orion Kidder', semester: 'Summer 2014', section: 'D100', role: 'Student',
 			courseMaterials: [
-				{ name: 'Introduction to Hamlet', id: 16, fileType: 'PDF', uploadDate: '2014-05-05', download: true, url: 'test/1.PDF'},
-				{ name: 'Different Types of Irony', id: 17, fileType: 'PDF', uploadDate: '2014-05-12', url: 'test/1.PDF'}
+				{ name: 'Introduction to Hamlet', id: 16, fileType: 'PDF', uploadDate: '2014-05-05', timestamp: '5:59PM', download: true, url: 'test/1.PDF'},
+				{ name: 'Different Types of Irony', id: 17, fileType: 'PDF', uploadDate: '2014-05-12', timestamp: '5:59PM', url: 'test/1.PDF'}
 			],
 			assignments: [
-				{name: 'Character Analysis', id: 26, dueDate: '2014-05-25', totalGrade: 100, weight: 25, uploadDate: '2014-05-05', url: 'test/1.PDF'},
-				{name: 'Term Paper', id: 28, dueDate: '2014-08-20', totalGrade: 10, weight: 30, uploadDate: '2014-05-05', url: 'test/1.PDF'}
+				{name: 'Character Analysis', id: 26, dueDate: '2014-05-25', timeDue: '11:59PM', totalGrade: 100, weight: 25, uploadDate: '2014-05-05', timestamp: '5:59PM', url: 'test/1.PDF'},
+				{name: 'Term Paper', id: 28, dueDate: '2014-08-20', timeDue: '11:59PM', totalGrade: 10, weight: 30, uploadDate: '2014-05-05', timestamp: '5:59PM', url: 'test/1.PDF'}
 			] },
 		],
 		submissions: [
-			{id: 40, assignmentId: 21, submitDate: '2014-05-25', status: 'submitted', actualGrade: 100, studentComment: 'Please give me a good mark, Herbert.', instructorComment: 'Well done', file: 'test/1.PDF'},
-			{id: 41, assignmentId: 22, submitDate: '2014-06-16', status: 'late', actualGrade: '', studentComment: 'Sorry, late!', instructorComment: '', file: 'test/1.PDF'},
-			{id: 42, assignmentId: 33, submitDate: '2014-06-16', status: 'submitted', actualGrade: '', studentComment: 'This was hard', instructorComment: '', file: 'test/1.PDF'},
-			{id: 43, assignmentId: 24, submitDate: '2014-06-16', status: 'unsubmitted', actualGrade: '', studentComment: '', instructorComment: '', file: 'test/1.PDF'},
-			{id: 44, assignmentId: 26, submitDate: '2014-06-16', status: 'submitted', actualGrade: 25, studentComment: 'I like hotdogs.', instructorComment: 'Please elaborate', file: 'test/1.PDF'},
+			{id: 40, assignmentId: 21, submitDate: '2014-05-25', timestamp: '3:59PM', status: 'submitted', actualGrade: 100, studentComment: 'Please give me a good mark, Herbert.', instructorComment: 'Well done', file: 'test/1.PDF'},
+			{id: 41, assignmentId: 22, submitDate: '2014-06-16', timestamp: '5:59PM', status: 'late', actualGrade: '', studentComment: 'Sorry, late!', instructorComment: '', file: 'test/1.PDF'},
+			{id: 42, assignmentId: 33, submitDate: '2014-06-16', timestamp: '9:59PM', status: 'submitted', actualGrade: '', studentComment: 'This was hard', instructorComment: '', file: 'test/1.PDF'},
+			{id: 43, assignmentId: 24, submitDate: '2014-06-16', timestamp: '2:59PM', status: 'unsubmitted', actualGrade: '', studentComment: '', instructorComment: '', file: 'test/1.PDF'},
+			{id: 44, assignmentId: 26, submitDate: '2014-06-16', timestamp: '10:59PM', status: 'submitted', actualGrade: 25, studentComment: 'I like hotdogs.', instructorComment: 'Please elaborate', file: 'test/1.PDF'},
 		]
 	};
 	
