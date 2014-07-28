d0155e8667d35ec603c04f46135f2f36f9249639
Daniel Chappell <chappell.daniel+github@gmail.com>
2014-07-27 19:26:39 -0700

    Added table to  attendance page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -101,12 +101,25 @@ nov.controller('AttendanceCtrl', ['$scope', '$stateParams', 'Data', 'userModel',
 	$scope.course = Data.getCourse($stateParams.courseId);
 	$scope.attendance = Data.getAttendance($stateParams.courseId);
 	$scope.index = 0;
+	$scope.cancelAttendance = function() {
+		while ($scope.index >= 0) {
+			$scope.course.studentList[$scope.index - 1].present = 'U';
+			$scope.index = $scope.index - 1;
+		};
+		$scope.increaseIndex();
+	};
+	$scope.increaseIndex = function() {
+		$scope.index = $scope.index + 1;
+	};
+	$scope.setIndex = function(ind) {
+		$scope.index = ind;
+	};
 	$scope.getDate = function() {
 		return date.getDate();
-	}
+	};
 	$scope.getTime = function() {
 		return date.getTime();
-	}
+	};
 }]);
 
 
